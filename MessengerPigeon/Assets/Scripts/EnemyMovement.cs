d4ab3add3455ce9a11f45cc98b3af90af9f85875
ASmithie <alecsmith_@outlook.com>
2022-02-24 15:05:14 +0000

    lose screen works now

--- MessengerPigeon/Assets/Scripts/EnemyMovement.cs
+++ MessengerPigeon/Assets/Scripts/EnemyMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -76,6 +77,7 @@ public class EnemyMovement : MonoBehaviour
         {
             Debug.Log("Caught");
             Debug.Log(heading);
+            SceneManager.LoadScene("LoseScreen");
         }
     }
 
